39f36fd44d231c62a546b40610dc31501901ecb4
jamil2388 <ahmed491@uwindsor.ca>
2024-04-01 15:13:30 -0400

    reshaped simlex preprocessing with lazy load applied

--- src/simlex.py
+++ src/simlex.py
@@ -18,78 +18,94 @@ import re
 import pandas as pd
 import numpy as np
 import nltk
+import pickle
+import os
 from nltk.corpus import brown
 
 simlex_filepath = '../data/SimLex-999.txt'
+simlex_preprocessed_filepath = '../data/simlex_prep.pkl'
 cols = ["word1", "word2", "SimLex999"]  # the columns to fetch from SimLex
+
+
+def load_file(filepath):
+    if os.path.exists(filepath):
+        with open(filepath, 'rb') as f:
+            return pickle.load(f)
+    return None
+
+def save_file(data, filepath):
+    with open(filepath, 'wb') as f:
+        pickle.dump(data, f)
 
 # load SimLex and return the df with the relevant columns
 def preprocess_simlex(filepath, cols):
-    # Read the file into a DataFrame, selecting only the required columns
-    df = pd.read_csv(filepath, sep="\t", usecols=cols)
-    return df
 
-# create a dict which stores the index of the unique words in the given col
-def create_word_index(df, col):
-    d = {}
-    for id, word in enumerate(df[col]):
-        if not d.get(word, 0):
-            d[word] = [id]
-        else:
-            d[word].append(id)
-    print(len(d.values()))
-    return d
+    df = pd.read_csv(filepath, sep="\t", usecols=cols) # Read the file into a DataFrame, selecting only the required columns
+    vocab = pd.Series(list(set(df['word1'].tolist() + df['word2'].tolist()))) # Step 1: Extract unique words from columns word1 and word2
+    sorted_df = df.sort_values(by='SimLex999', ascending=False) # Sort DataFrame by rating SimLex999 ratings in descending order
+    simlex_vocab = pd.DataFrame({'word': vocab, 'topk' : pd.Series(), 'sim_rating' : pd.Series()})
+
+    return sorted_df, simlex_vocab
 
 # create a column which contains the top 10 most similar word indices
-def calculate_topk(df, k=10):
-    topk_indices = []
-    for idx, row in df.iterrows():
-        current_word = row["word1"]
-        # Find the indices of top k most similar words based on SimLex999 ratings
-        similar_indices = df[df["word1"] == current_word]["SimLex999"].nlargest(k).index.tolist()
-        topk_indices.append(set(similar_indices))
-    return topk_indices
+# the dataframe should already be sorted
+def add_topk(df, simlex_vocab, k=10):
+    for row_id, row in simlex_vocab.iterrows():
 
+        word = row['word']
+        similar_words, ratings = calculate_topk(df, word)
+        # update the values in the particular row_id
+        simlex_vocab.loc[row_id, 'topk'] = list(similar_words) # first initialization of values
+        simlex_vocab.loc[row_id, 'sim_rating'] = list(ratings) # first initialization of values
 
-# Function to sort each set in the 'topk' column based on 'SimLex999' values
-def sort_topk_with_simlex(df, row):
-    # List to store SimLex999 values corresponding to elements in 'topk'
-    lst = [df.iloc[id]['SimLex999'] for id in row['topk']]
-    # Sort 'topk' and lst based on descending order of lst
-    sorted_pairs = sorted(zip(row['topk'], lst), key=lambda x: x[1], reverse=True)
-    sorted_topk = [pair[0] for pair in sorted_pairs]
-    return sorted_topk
-
-# fill the 'topk' col with k values
-def fill_topk(df, word2_index):
-    col_topk = [{-1} for _ in range(len(df))] # create a col for final topk results, then append the col in the df
-    for id in range(len(df)):
-        row = df.iloc[id][:]
-        bk_topk = row['topk']
-        # skip if there are already k indices in this row
         if len(row['topk']) < 10:
-            for id2 in bk_topk:
-                word2 = df.iloc[id2]['word2'] # get the similar word from this row
-                print(word2)
-                row['topk'] = row['topk'].union(word2_index[word2])
-            # sort the ids in the row['topk'] based on the scores in the corresponding col
-            row['topk'] = sort_topk_with_simlex(df, row)
-            for id_w1 in bk_topk:
-                 col_topk[id_w1] = list(row['topk'])[:10] # filter out only the topk results and store it in topk col (col index = 3)
-    col_topk = pd.Series(col_topk)
-    col_topk.reset_index(drop = True, inplace = True)
-    df['topk'] = col_topk
-    return df
+            word2 = word
+            current_topk = row['topk'].copy() # we need to iterate through a static list, and update the row['topk'] dynamically
+            for word1 in current_topk:
+                similar_words, ratings = calculate_topk(df, word1, word2)
 
-def prepare_simlex():
+                if similar_words is not None and ratings is not None :
+                    # update the values in the particular row_id
+                    simlex_vocab.loc[row_id, 'topk'].extend(list(similar_words))  # first initialization of values
+                    simlex_vocab.loc[row_id, 'sim_rating'].extend(list(ratings))  # first initialization of values
 
-    simlex_df = preprocess_simlex(simlex_filepath, cols)
-    word1_index = create_word_index(simlex_df, 'word1')  # create the index of the unique words in col 'word1'
-    word2_index = create_word_index(simlex_df, 'word2')  # create the index of the unique words in col 'word2'
-    simlex_df['topk'] = calculate_topk(simlex_df,10)  # list the topk words for each word in word1 col and create a col named 'topk'
-    simlex_df = fill_topk(simlex_df, word2_index)  # fill the topk values for rows which have less than k values
+        # truncate the list to topk words
+        simlex_vocab.loc[row_id, 'topk'] = simlex_vocab.loc[row_id, 'topk'][:10]
+        simlex_vocab.loc[row_id, 'sim_rating'] = simlex_vocab.loc[row_id, 'sim_rating'][:10]
 
-    return simlex_df
+    return simlex_vocab
+
+# returns the most similar words to word1 having word2 absent in all of the cases (because we already populated that one)
+def calculate_topk(df, word1, word2 = None):
+
+    # Filter rows where word1 or word2 matches the current word
+    filtered_df = df.loc[(df['word1'] == word1) | (df['word2'] == word1)] if not word2 else df[((df['word1'] == word1) & (df['word2'] != word2)) | ((df['word2'] == word1) & (df['word1'] != word2))]
+
+    if len(filtered_df) == 0: return None, None
+
+    # Take top 10 most similar words and their ratings
+    top_10_similar_words = filtered_df.head(10)[['word1', 'word2', 'SimLex999']]
+    # Convert DataFrame rows to a list of tuples (word, rating)
+    similar_word_rating_pairs = [
+        (row['word1'], row['SimLex999']) if row['word2'] == word1 else (row['word2'], row['SimLex999']) for
+        index, row in top_10_similar_words.iterrows()]
+
+    # Separate similar words and ratings into two lists
+    similar_words, ratings = zip(*similar_word_rating_pairs)
+
+    return similar_words, ratings
+
+# lazy load the simlex preprocessed dataframe
+def prepare_simlex(filepath = None):
+
+    data = load_file(filepath)
+    if data is not None: return data
+
+    simlex_df, simlex_vocab = preprocess_simlex(simlex_filepath, cols) # simlex_df = original df, simlex_vocab = unique words and their topk similar words and ratings
+    simlex_vocab = add_topk(simlex_df, simlex_vocab, 10)  # append new cols topk and sim_rating based showing lists of topk words to the unique word in col 'word'
+    save_file(simlex_vocab, filepath if filepath else simlex_preprocessed_filepath)
+
+    return simlex_vocab
 
 if __name__ == '__main__':
-    prepare_simlex()
+    prepare_simlex(simlex_preprocessed_filepath)
